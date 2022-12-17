be71cdc43e294ca0435f93fa4d3660c43036c485
dprindi16 <d.m.prindiville@wustl.edu>
2022-12-16 23:48:53 -0600

    Complete

--- functions.js
+++ functions.js
@@ -16,30 +16,37 @@ clickSearch.addEventListener("click", () => {
                 .then((stats) => {
                     output.innerHTML= `
                         <div class = "statistics">
-                            <p class = "player-info"> ${data.data[0].first_name} ${data.data[0].last_name} • ${data.data[0].team.full_name} • Position: ${data.data[0].position}</p>
-                            <div class = "row-one"> 
+                            <p class = "player-info"> ${data.data[0].first_name} ${data.data[0].last_name} | ${data.data[0].team.full_name} | Position: ${data.data[0].position}</p>
+                            <div class = "main-stats">
+                            <div class = "column"> 
                                 <p>Games Played: ${stats.data[0].games_played}</p>
                                 <p>Minutes: ${stats.data[0].min}</p>
                                 <p>Points: ${stats.data[0].pts}</p>
-                            </div>
-                            <div class = "row-two"> 
                                 <p>Assists: ${stats.data[0].ast}</p>
-                                <p>Defensive Rebounds: ${stats.data[0].dreb}</p>
-                                <p>Offensive Rebounds: ${stats.data[0].oreb}</p>
                             </div>
-                            <div class = "row-three"> 
+                            <div class = "column"> 
+                                <p>Defensive Rebs: ${stats.data[0].dreb}</p>
+                                <p>Turnovers: ${stats.data[0].turnover}</p>
                                 <p>Steals: ${stats.data[0].stl}</p>
                                 <p>Blocks: ${stats.data[0].blk}</p>
-                                <p>Turnovers: ${stats.data[0].turnover}</p>
                             </div>
-                            <div class = "row-four"> 
-                                <p>Field Goal %: ${stats.data[0].fg_pct}</p>
-                                <p>3 Point %: ${stats.data[0].fg3_pct}</p>
-                                <p>Free Throw %: ${stats.data[0].ft_pct}</p>
+                            <div class = "column"> 
+                                <p>Offensive Rebs: ${stats.data[0].oreb}</p>
+                                <p>Field Goal: ${stats.data[0].fg_pct}</p>
+                                <p>3 Point: ${stats.data[0].fg3_pct}</p>
+                                <p>Free Throw: ${stats.data[0].ft_pct}</p>
+                            </div>
                             </div>
                         </div>
                             `;
                     console.log(stats.data[0]);
+                })
+                .catch(() => {
+                    if (playerName.length == 0){
+                        output.innerHTML = `<h2>Please enter a name</h2>`;
+                    } else{
+                        output.innerHTML = `<h2>Please enter the full name of an NBA player name who played in 2021-2022</h2>`;
+                    }
                 })
     }  )
 });
